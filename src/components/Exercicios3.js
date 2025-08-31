0574d6aed85d4a6c63c6fe6146fa3d41ab44fd33
JVamarante <joaovamarante@gmail.com>
2025-08-31 10:49:28 -0300

    commit inicial

---
+++ src/components/Exercicios3.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Exercicios3 = () => {
+  return (
+    <View>
+      <Text>Exercicios3</Text>
+    </View>
+  )
+}
+
+export default Exercicios3
